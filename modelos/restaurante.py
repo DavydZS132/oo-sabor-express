d7cddc5aa8d0135510bef627d717855ef7e6c211
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-03-15 12:25:24 -0300

    Metodos Especiais

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,10 +1,15 @@
 class Restaurante:
-    def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+    def __init__(this, nome, categoria):
+        this.nome = nome
+        this.categoria = categoria
+        this.ativo = False
+    def __str__(this):
+        return f'{this.nome} | {this.categoria}'
 
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante('Artur', 'Lanches')
+restaurante_pizza = Restaurante('Pizza Expres', 'Italiano')
 
 restaurante = [restaurante_praca, restaurante_pizza]
+
+print(restaurante_praca)
+print(restaurante_pizza)
