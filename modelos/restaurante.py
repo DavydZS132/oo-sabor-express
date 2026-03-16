abbc50ea483487793ed22ffceae0930468b15175
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-03-15 21:05:33 -0300

    Criando meus Métodos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,15 +1,20 @@
 class Restaurante:
+    restaurantes = []
     def __init__(this, nome, categoria):
         this.nome = nome
         this.categoria = categoria
         this.ativo = False
+        Restaurante.restaurantes.append(this)
+
     def __str__(this):
         return f'{this.nome} | {this.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+
 
 restaurante_praca = Restaurante('Artur', 'Lanches')
 restaurante_pizza = Restaurante('Pizza Expres', 'Italiano')
 
-restaurante = [restaurante_praca, restaurante_pizza]
-
-print(restaurante_praca)
-print(restaurante_pizza)
+Restaurante.listar_restaurantes()
