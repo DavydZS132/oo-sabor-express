ad9b7a8080dc2f654a5224fd8257a1339b97b786
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-03-16 11:43:27 -0300

    Refatorando uma função

--- mao-na-massa/musica.py
+++ mao-na-massa/musica.py
@@ -10,3 +10,13 @@ artista_jobs.artista = 'Raper'
 artista_jobs.duracao = 12
 
 print(vars(artista_jobs))
+
+class Musica:
+    def __init__(self, nome='', artista='', duracao=0):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
+
+musica1 = Musica(nome='Under Pressure', artista='Queen & David Bowie', duracao=248)
+musica2 = Musica(nome='The Trooper', artista='Iron Maiden', duracao=245)
+musica3 = Musica(nome='Hotel California', artista='Eagles', duracao=390)
