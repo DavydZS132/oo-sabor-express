15c72e63933800427fcb1696f993542b9a64ed19
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-03-20 11:31:02 -0300

    Property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,18 +1,22 @@
 class Restaurante:
     restaurantes = []
-    def __init__(this, nome, categoria):
-        this.nome = nome
-        this.categoria = categoria
-        this.ativo = False
-        Restaurante.restaurantes.append(this)
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
-    def __str__(this):
-        return f'{this.nome} | {this.categoria}'
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes():
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria do Restaurante'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
+    @property
+    def ativo(self):
+        return ' Verdadeiro' if self._ativo else 'falso'
 
 restaurante_praca = Restaurante('Artur', 'Lanches')
 restaurante_pizza = Restaurante('Pizza Expres', 'Italiano')
