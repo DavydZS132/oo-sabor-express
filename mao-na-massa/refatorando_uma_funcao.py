cbef8226e22c5f2185a8c9f4272761d6abe61cb2
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-03-21 23:06:10 -0300

    Refatorando Uma Função

---
+++ mao-na-massa/refatorando_uma_funcao.py
@@ -0,0 +1,46 @@
+# Crie uma nova classe chamada Pessoa com atributos como nome, idade e profissão. Adicione um método especial __str__ para imprimir uma representação em string da pessoa. Implemente também um método de instância chamado aniversario que aumenta a idade da pessoa em um ano. Por fim, adicione uma propriedade chamada saudacao que retorna uma mensagem de saudação personalizada com base na profissão da pessoa.
+class Livro:
+    def __init__(self, titulo='', autor='', paginas=0):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def __str__(self):
+        return f'{self.titulo} por {self.autor} - {self.paginas} páginas'
+
+    @property
+    def titulo_autor(self):
+        return f'{self.titulo} por {self.autor}'
+
+    def aumentar_paginas(self, quantidade):
+        self.paginas += quantidade
+
+class Pessoa:
+    def __init__(self, nome, idade, profissao):
+        self.nome = nome
+        self.idade = idade
+        self.profissao = profissao
+
+    def __str__(self):
+        return f'{self.nome} | {self.idade} | {self.profissao}'
+    
+    @property
+    def saudacao(self):
+        if self.profissao:
+            return f'Olá, sou {self.nome}! Trabalho como {self.profissao}.'
+        else:
+            return f'Olá, sou {self.nome}!'
+        
+    def data_aniversario(self):
+        self.idade += 1
+
+
+pessoa1 = Pessoa(nome='Alice', idade=25, profissao='Engenheira')
+print("Informações Iniciais:")
+print(pessoa1)
+
+pessoa1.data_aniversario()
+print("Informações após aniversário:")
+print(pessoa1)
+
+print(pessoa1.saudacao)
