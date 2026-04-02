94661381812013da2c2109a79b41654944358190
Davyd Zuquim Silva <dzuquim10@gmail.com>
2026-04-02 12:03:32 -0300

    Media Formatada e Receber Avaliação

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -16,7 +16,13 @@ class Restaurante:
     def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria do Restaurante'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_formatada).ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def media_formatada(self):
+        if not self._avaliacao:
+            return 'Restaurante novo'
+        return self.media_avaliacoes
 
     @property
     def ativo(self):
@@ -26,13 +32,19 @@ class Restaurante:
         self._ativo = not self._ativo
 
     def receber_avaliacao(self, cliente, nota):
-        avaliacao = Avaliacao(cliente, nota)
-        self._avaliacao.append(avaliacao)
+        # Verifique se a nota está entre 0 e 5
+        if 0 <= nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+        else:
+            # Opcional: avisar que a nota foi ignorada
+            print(f'Nota {nota} inválida! Digite um valor entre 0 e 5.')
 
     @property
     def media_avaliacoes(self):
         if not self._avaliacao:
             return 0
+        
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
